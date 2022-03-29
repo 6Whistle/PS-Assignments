a971272017bbf985b39873a4abb7ad24043862aa
6Whistle <camjun0506@icloud.com>
2022-03-30 03:15:40 +0900

    3.29

--- assignement1.cpp
+++ assignement1.cpp
@@ -1,8 +1,35 @@
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
+
+
+using namespace std;
+
+double Gaussian_distribution(double ave, double dev);
 
 int main(void){
-    int ave, dev;
+    srand((unsigned int)time(NULL));
 
-    
+    double ave, dev;
+    double array[10];
+
+    cin >> ave >> dev;
+    for(int i = 0; i < 10; i++){
+        array[i] = Gaussian_distribution(ave, dev);
+        cout << array[i] << '\n';
+    }
+    system("pause");
+
+}
+
+double Gaussian_distribution(double ave, double dev){
+    double x = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+    cout << x << " ";
+
+    return exp(pow((x - ave), 2) / (-2 * dev)) / sqrt(2 * M_PI * dev);
+
 }
