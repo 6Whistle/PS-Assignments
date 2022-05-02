7de6bb8e1cb792c65a5cfb4044c7cfb81213be7c
6Whistle <camjun0506@icloud.com>
2022-05-02 18:29:42 +0900

    update

--- assignement2_2/encoder.cpp
+++ assignement2_2/encoder.cpp
@@ -64,14 +64,15 @@ class Tree{
                 q.push(temp->getLeft());
             if(temp->getRight())
                 q.push(temp->getRight());
-            delete temp;
+            if(temp)
+                delete temp;
         }
     }
 
     Node* getRoot(){ return root; }
     void setRoot(Node* i_root){root = i_root; }
 
-    //Making BST using priority queue
+    //Making each character's BST using priority queue
     void makeTree(map<char, int> input){
         priority_queue<Node *, vector<Node *>,  Node> pq;   //priority queue(sort : greater)
         //push all leaf data in queue
@@ -94,6 +95,7 @@ class Tree{
         root = pq.top();
     }
 
+    //Making BST using priority queue
     void makeTree(map<char, pair<int, map<char, int> > > input){
         priority_queue<Node *, vector<Node *>,  Node> pq;   //priority queue(sort : greater)
         //push all leaf data in queue
@@ -124,7 +126,7 @@ class Tree{
 class Huffman{
     private :
     map<char, pair<uint8_t, bitset<16> > > table;       //Huffman table
-    map<char, map<char, pair<uint8_t, bitset<16> > > > charTable;     //Character's Huffman table
+    map<char, map<char, pair<uint8_t, bitset<16> > > > charTable;     //Context adaptive Huffman table
 
     public :
     Huffman(){};
@@ -140,6 +142,7 @@ class Huffman{
             makeHuffmanTable(node->getRight(), pattern.set(16 - size, 1), size);   //pattern add 1
     }
 
+    //Make each character's table using BST
     void makeCharHuffmanTable(char ch, Node* node, bitset<16> pattern, uint8_t size){
         if(node->getChar())         //if current node is leaf, save leaf's data in Huffman table
             charTable[ch][node->getChar()] = make_pair(size, pattern);
@@ -151,40 +154,48 @@ class Huffman{
             makeCharHuffmanTable(ch, node->getRight(), pattern.set(16 - size, 1), size);   //pattern add 1
     }
 
-    void Make(map<char, pair<int, map<char, int> > > prob){
+    //Make Huffman table and context adaptive Huffman table
+    void makeTable(map<char, pair<int, map<char, int> > > prob){
         Tree bst;
-        map<char, Tree> char_bst;
+        map<char, Tree *> char_bst;
         bitset<16> pattern = 0;
         uint8_t size = 0;
 
+        //Make Tree
         bst.makeTree(prob);
-        for(auto i : prob)
-            char_bst[i.first].makeTree(i.second.second);
+        for(auto i : prob){
+            char_bst[i.first] = new Tree();
+            char_bst[i.first]->makeTree(i.second.second);
+        }
 
-        
+        //Write Table using Tree
         this->makeHuffmanTable(bst.getRoot(), pattern, size);      
-        pattern.reset();
-        size = 0;
-        for(auto i : charTable)
-            this->makeCharHuffmanTable(i.first, char_bst[i.first].getRoot(), pattern, size);
-        
+        for(auto i : char_bst){
+            pattern.reset();
+            size = 0;
+            this->makeCharHuffmanTable(i.first, char_bst[i.first]->getRoot(), pattern, size);
+        }
+
+        //delete Tree
+        for(auto i : prob)
+            delete char_bst[i.first];
+
     }
 
 
     //Print Huffman table's data
     void printTable(){
         for(auto i : table){
-            cout << "Table >> " << i.first << " : size-" << (unsigned int)(i.second.first) << ", pattern-" << i.second.second << endl;
+            cout << "Table >> " << i.first << " : size-" << (unsigned int)(i.second.first) << ", pattern-" << i.second.second
+                 << ", number-" << charTable[i.first].size() << endl;
             for(auto j : charTable[i.first])
                 cout << j.first << " : size-" << (unsigned int)(j.second.first) << ", pattern-" << j.second.second << "\t";
-            cout << endl;
+            cout << endl << endl;
         }   
     }
 
-    //Write table file and encode input file
-    void encoding(const char* input_file_path){
-        ofstream write_table_file, write_file;
-        ifstream read_file;
+    void writeTableFile(const char* table_file_path){
+        ofstream write_table_file, write_adaptive_table_file;
         unsigned int buf = 0;       //32 bits buffer
         int max = 0;                //buffer's current size
 
@@ -201,8 +212,10 @@ class Huffman{
                 if(max == 32)
                     b = 0;
                 buf = buf | b;
-                for(int i = 3; i >=0 ;i--)      
-                    write_table_file << (unsigned char)(buf >> i * 8);
+                for(int i = 3; i >=0 ;i--){            //write last element in buffer
+                    char a = (unsigned char)(buf >> (i*8));
+                    write_table_file.write(&a, 1);        
+                }
                 buf = temp << (32 - max);       //buf = temp's not written bits
                 max = max + t_size - 32;        //change buffer's size
             }
@@ -211,38 +224,22 @@ class Huffman{
                 max = t_size + max;             //change buffer's size
             }
 
-            /* input size of Character Huffman */
-            temp = (unsigned int)charTable[i.first].size() << 24;
-            t_size = 8;
-            if(max + t_size > 32){      //if buffer + temp size > 32 bits
-                unsigned int b = temp >> max;       //write 32 bit buffer
-                if(max == 32)
-                    b = 0;
-                buf = buf | b;
-                for(int i = 3; i >=0 ;i--)      
-                    write_table_file << (unsigned char)(buf >> i * 8);
-                buf = temp << (32 - max);       //buf = temp's not written bits
-                max = max + t_size - 32;        //change buffer's size
-            }
-            else{
-                buf = buf | (temp >> max);      //buffer = [buffer|temp]
-                max = t_size + max;             //change buffer's size
-            }
-
-            /* input Character Huffman */
-            for(auto j : charTable){
-                temp = (unsigned int)(i.first) << 8;       //temp = [character|size|pattern]
-                temp = (temp | (unsigned int)(i.second.first)) << 16;
-                temp = temp | (unsigned int)(i.second.second.to_ulong());
-                t_size = 16 + (int)(i.second.first);                //temp's size
+            /* input Character table */
+            for(auto j : charTable[i.first]){
+                temp = (unsigned int)(j.first) << 8;       //temp = [character|size|pattern]
+                temp = (temp | (unsigned int)(j.second.first)) << 16;
+                temp = temp | (unsigned int)(j.second.second.to_ulong());
+                t_size = 16 + (int)(j.second.first);                //temp's size
 
                 if(max + t_size > 32){      //if buffer + temp size > 32 bits
                     unsigned int b = temp >> max;       //write 32 bit buffer
                     if(max == 32)
                         b = 0;
                     buf = buf | b;
-                    for(int i = 3; i >=0 ;i--)      
-                        write_table_file << (unsigned char)(buf >> i * 8);
+                    for(int i = 3; i >=0 ;i--){            //write last element in buffer
+                        char a = (unsigned char)(buf >> (i*8));
+                        write_table_file.write(&a, 1);        
+                    }
                     buf = temp << (32 - max);       //buf = temp's not written bits
                     max = max + t_size - 32;        //change buffer's size
                 }
@@ -252,24 +249,79 @@ class Huffman{
                 }
             }
         }
+        unsigned int temp = 127 << 24;      //end of table insert
+        int t_size = 8;
+        if(max + t_size > 32){      //if buffer + temp size > 32 bits
+            unsigned int b = temp >> max;       //write 32 bit buffer
+            if(max == 32)
+                b = 0;
+            buf = buf | b;
+            for(int i = 3; i >=0 ;i--){            //write last element in buffer
+                char a = (unsigned char)(buf >> (i*8));
+                write_table_file.write(&a, 1);        
+            }
+            buf = temp << (32 - max);       //buf = temp's not written bits
+            max = max + t_size - 32;        //change buffer's size
+        }
+        else{
+            buf = buf | (temp >> max);      //buffer = [buffer|temp]
+            max = t_size + max;             //change buffer's size
+        }
         for(int i = 3; i >= 0; i--){            //write last element in buffer
             char a = (unsigned char)(buf >> (i*8));
-            write_file.write(&a, 1);        
+            write_table_file.write(&a, 1);        
         }
         write_table_file.close();
 
+    }
+
+    int AddBuffer(ofstream &write_table_file, unsigned int buf, int max, unsigned int temp, int t_size){
+        if(max + t_size > 32){      //if buffer + temp size > 32 bits
+            unsigned int b = temp >> max;       //write 32 bit buffer
+            if(max == 32)
+                b = 0;
+            buf = buf | b;
+            for(int i = 3; i >=0 ;i--){            //write last element in buffer
+                char a = (unsigned char)(buf >> (i*8));
+                write_table_file.write(&a, 1);        
+            }
+            buf = temp << (32 - max);       //buf = temp's not written bits
+            max = max + t_size - 32;        //change buffer's size
+        }
+        else{
+            buf = buf | (temp >> max);      //buffer = [buffer|temp]
+            max = t_size + max;             //change buffer's size
+        }
+        return max;
+    }
+
+    //Write table file and encode input file
+    void encoding(const char* input_file_path, const char* code_file_path){
+        ofstream write_table_file, write_file;
+        ifstream read_file;
+        unsigned int buf = 0;       //32 bits buffer
+        int max = 0;                //buffer's current size
+
         //encode input file
         read_file.open(input_file_path);        //input file
-        write_file.open("huffman_code.hbs", ios::binary | ios::trunc);      //write encoding file new and binary mode
+        write_file.open(code_file_path, ios::binary | ios::trunc);      //write encoding file new and binary mode
+        char ch_before = 0;
         char ch = 0;                //read char from input file
         buf = 0;                    //32 bit buffer
         max = 0;                    //buffer's current size
         bool endf = false;          //Check end file
 
+        //read 1byte and input transformed pattern using Huffman table
+        if(read_file.get(ch).eof())
+            return;
+        buf = table[ch].second.to_ulong() << 16;
+        max = table[ch].first;
+
+        //read and input transformed pattern using Context adaptive huffman table
         while(true){
             unsigned int temp;
             unsigned int t_size;
-
+            ch_before = ch;         //before character
             read_file.get(ch);      //read character
             if(read_file.eof()){    //if read file is end, character = EOD & end file = true
                 endf = true;
@@ -277,8 +329,8 @@ class Huffman{
             }
 
             //character's table value
-            temp = table[ch].second.to_ulong() << 16;
-            t_size = table[ch].first;
+            temp = charTable[ch_before][ch].second.to_ulong() << 16;
+            t_size = charTable[ch_before][ch].first;
 
             //if buffer and temp size > 32 bit
             if(max + t_size > 32){
@@ -314,50 +366,31 @@ class Huffman{
 
 int main(){
     ifstream input_file;
-    input_file.open("training_input.txt");      //open input_data.txt
-    map<char, int> ascii_count;             //character's probability
     map<char, pair<int, map<char, int> > > next_ascii_count;
-    for(auto i : next_ascii_count){
-        i.second.first = 0;
-        for(auto j : i.second.second){
-            j.second = 0;
-        }
-    }
+    char c, c_before;
 
+    input_file.open("training_input.txt");      //open input_data.txt
     if(!input_file.is_open()){
         cout << "File open error" <<endl;
         return -1;
     }
-    
-    char c, c_before;
-    if(input_file.get(c).eof()){
-        printf("No Data in traing file");
+    if(input_file.get(c).eof()){                        //read first character and add probability
+        printf("No Data in training file");
         return -1;
     }
     (next_ascii_count[c].first)++;
-    c_before = c;
-    while(!input_file.get(c).eof()){               //count each character's probability in input_data
+    c_before = c;                                       //save before character
+    while(!input_file.read(&c, 1).eof()){               //count each character's probability in train_input.txt data
         next_ascii_count[c_before].second[c]++;
         (next_ascii_count[c].first)++;
-        c_before = c;
+        c_before = c;                                   //save before character
     }
-    next_ascii_count[c_before].second[127]++;
+    next_ascii_count[c_before].second[127]++;           //add EOF
     input_file.close();
 
-    for(auto i : next_ascii_count){
-        if(i.second.first != 0){
-            cout << i.first << endl;
-            for(auto j : i.second.second){
-                if(j.second != 0)
-                    cout << j.first << " : " << j.second << "\t";
-            }
-            cout << endl;
-        }
-    }
-
+    //Make train_input
     Huffman huf;
-    huf.Make(next_ascii_count);
+    huf.makeTable(next_ascii_count);
     huf.printTable();
-
-    int i = 0;
+    huf.encoding("training_input.txt", "training_input_code.hbs");
 }
